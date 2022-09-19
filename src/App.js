044a7daa6e2187e3c0b180a41177149116161eb4
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2022-09-19 18:45:06 +0530

    feat: added main source code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import { BryntumCalendar } from '@bryntum/calendar-react';
-import { calendarConfig } from './CalendarConfig';
-import './App.scss';
+import { useEffect, useState } from "react";
+import { BryntumCalendar } from "@bryntum/calendar-react";
+import { gapi } from "gapi-script";
+import "./App.scss";
 
-function App() {
-    return (
-        <BryntumCalendar {...calendarConfig} />
-    );
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+const calendarID = process.env.REACT_APP_CALENDAR_ID;
+
+const calendarConfig = {
+  date: new Date(),
+};
+
+export default function App() {
+  const [events, setEvents] = useState([]);
+
+  const getEvents = () => {
+    function initiate() {
+      gapi.client
+        .init({
+          apiKey: apiKey,
+        })
+        .then(function () {
+          return gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
+              calendarID
+            )}/events`,
+          });
+        })
+        .then(
+          (response) => {
+            let events = response.result.items;
+            setEvents(
+              events.map((event) => ({
+                id: event.id,
+                name: event.summary,
+                startDate: event.start.date,
+                endDate: event.end.date,
+              }))
+            );
+          },
+          function (err) {
+            console.error(err.body);
+          }
+        );
+    }
+    gapi.load("client", initiate);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return <BryntumCalendar events={events} {...calendarConfig} />;
 }
-
-// If you plan to use stateful React collections for data binding please check this guide
-// https://www.bryntum.com/docs/calendar/guide/Calendar/integration/react/data-binding
-
-export default App;
